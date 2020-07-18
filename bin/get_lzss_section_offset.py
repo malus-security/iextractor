52b7aa684ed1f25a7815060aca7ef6228cf6ae6f
Andrei-Calin Georgescu <calinandrei8@gmail.com>
2020-07-18 15:15:32 +0300

    Update get_lszz_section_offset.py to use Python3
    Use Python3 in get_lszz_section_offset.py and call it using python3
    from bin/decrypt_kernel.

--- bin/get_lzss_section_offset.py
+++ bin/get_lzss_section_offset.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import struct
@@ -10,7 +10,7 @@ def main():
         sys.exit(1)
 
     offset = 0
-    with open(sys.argv[1]) as f:
+    with open(sys.argv[1], 'rb') as f:
         found = False
         while True:
             buf = f.read(16)
@@ -19,11 +19,11 @@ def main():
             if len(buf) < 16:
                 break
             for i in range(0, 16):
-                if ord(buf[i]) == 0xff and i+4 < 16:
+                if buf[i] == 0xff and i+4 < 16:
                     chunk = struct.unpack("<I", buf[i+1:i+5])[0]
                     if chunk == 0xfeedface or chunk == 0xfeedfacf:
                         found = True
-                        print offset+i
+                        print(offset+i)
                         break
             if found == True:
                 break
